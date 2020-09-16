643c02679bcf455af9c0b73f16d3012f448f34dd
Kevin Lin <klin1234@gmail.com>
2020-09-16 15:53:23 -0400

    Starting to add tests

--- dvisR/R/pairs_management.R
+++ dvisR/R/pairs_management.R
@@ -1,11 +1,12 @@
 # existing_pairs is a vector of pairs already looked at. this hash object is passed by reference
 .generate_new_pairs <- function(p, new_pairs_per_round, existing_hash){
-  stopifnot(class(existing_hash) == "hash", !is.null(existing_hash[["count"]]))
+  stopifnot(class(existing_hash) == "hash", !is.null(existing_hash[["count"]]), 
+            length(existing_hash) + new_pairs_per_round - 1 <= p*(p-1)/2)
   
   counter <- 1
   pairs_mat <- matrix(NA, ncol = 2, nrow = new_pairs_per_round)
   
-  while(counter < new_pairs_per_round){
+  while(counter <= new_pairs_per_round){
     pair_vec <- sort(sample(p, 2))
     string_vec <- paste0(pair_vec, collapse = "-")
     
---
+++ dvisR/tests/testthat/test_pairs_management.R
@@ -0,0 +1,90 @@
+context("Test pairs management")
+
+## .generate_new_pairs is correct
+
+test_that(".generate_new_pairs works", {
+ set.seed(10)
+ p <- 100
+ new_pairs_per_round <- 10
+ h <- hash::hash()
+ .initialize_hash(h)
+ 
+ res <- .generate_new_pairs(p, new_pairs_per_round, h)
+ 
+ expect_true(is.matrix(res))
+ expect_true(all(dim(res) == c(10, 2)))
+ expect_true(all(!is.na(res)))
+})
+
+test_that(".generate_new_pairs affects h", {
+ set.seed(10)
+ p <- 100
+ new_pairs_per_round <- 10
+ h <- hash::hash()
+ .initialize_hash(h)
+ 
+ res <- .generate_new_pairs(p, new_pairs_per_round, h)
+ 
+ expect_true(length(h) == 1 + new_pairs_per_round)
+})
+
+test_that(".generate_new_pairs affects h", {
+ set.seed(10)
+ p <- 100
+ new_pairs_per_round <- 10
+ h <- hash::hash()
+ .initialize_hash(h)
+ 
+ res <- .generate_new_pairs(p, new_pairs_per_round, h)
+ 
+ expect_true(length(h) == 1 + new_pairs_per_round)
+})
+
+test_that(".generate_new_pairs generates h with unique values and unique keys", {
+ set.seed(20)
+ p <- 100
+ new_pairs_per_round <- 10
+ h <- hash::hash()
+ .initialize_hash(h)
+ 
+ # run it twice
+ res <- .generate_new_pairs(p, new_pairs_per_round, h)
+ res <- .generate_new_pairs(p, new_pairs_per_round, h)
+ 
+ expect_true(length(unique(hash::keys(h))) == length(hash::keys(h)))
+ tmp <- hash::values(h); tmp <- as.numeric(tmp[-which(names(tmp) == "count")])
+ expect_true(length(unique(tmp)) == length(tmp))
+})
+
+test_that(".generate_new_pairs errors if you ask for too many pairs", {
+ set.seed(20)
+ p <- 5
+ new_pairs_per_round <- 7
+ h <- hash::hash()
+ .initialize_hash(h)
+ 
+ # run it twice
+ res <- .generate_new_pairs(p, new_pairs_per_round, h)
+ expect_error(.generate_new_pairs(p, new_pairs_per_round, h))
+ 
+ # or run it once
+ h <- hash::hash()
+ .initialize_hash(h)
+ expect_error(.generate_new_pairs(p, 2*new_pairs_per_round, h))
+})
+
+#################
+
+## .initialize_hash is correct
+
+test_that(".initialize_hash works", {
+ h <- hash::hash()
+ .initialize_hash(h)
+ 
+ expect_true(length(h) == 1)
+ expect_true(hash::keys(h) == "count")
+ expect_true(h[["count"]] == 0)
+})
+
+#################
+
