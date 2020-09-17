e25b08dcc014a2dfc97aa8dbd7769607fbf5f409
Kevin Lin <klin1234@gmail.com>
2020-09-17 16:11:31 -0400

    About to start testing dvisR

--- dvisR/R/dvisR.R
+++ dvisR/R/dvisR.R
@@ -8,8 +8,9 @@ dvisR_system <- function(dat, cluster_labels = rep(NA, nrow(dat)),
  if(!is.all(is.na(cluster_labels))) stopifnot(nrow(dat) == length(cluster_labels))
  .check_holistic(dat, plotting_options)
   
- fl <- feature_list; so <- system_options; po <- plotting_options; di <- debugging_inputs; pm <- plotting_module
- p <- ncol(dat)
+ fl <- feature_list; so <- system_options; po <- plotting_options; pm <- plotting_module
+ di <- debugging_inputs
+ n <- nrow(dat); p <- ncol(dat)
  
  # preprocessing, initialization stuff
  hash_pairs <- .initialize_hash(); hash_na <- .initialize_na_handler()
@@ -17,6 +18,7 @@ dvisR_system <- function(dat, cluster_labels = rep(NA, nrow(dat)),
                                            so$minimum_instances_first_phase, names(fl))
  pairs_mat <- matrix(NA, nrow = 0, ncol = 2)
  response_vec <- rep(NA, nrow(feature_mat))
+ round_vec <- rep(NA, nrow(feature_mat))
 
  round_idx <- 1
  phase_counter <- 1
@@ -25,13 +27,19 @@ dvisR_system <- function(dat, cluster_labels = rep(NA, nrow(dat)),
  
  # enter the main loop of the procedure
  while(!all(phase_completed)){
-  # sample new pairs :: need a check to ensure there are new pairs still
+  stopifnot(nrow(feature_mat) == length(response_vec), length(response_vec) == length(round_vec)) 
+   
+  # sample new pairs
   counter <- hash[["count"]]
-  stopifnot(is.na(feature_mat[counter+1,1]))
+  # check to see if we need to expand feature_mat and others
+  if(!is.na(feature_mat[counter+1,1])){
+    feature_mat <- .expand_feature_matrix(feature_mat)
+    response_vec <- .expand_response_vec(response_vec)
+    round_vec <- .expand_response_vec(round_vec)
+  }
   pairs_mat_new <- .generate_new_pairs(p, so$new_pairs_per_round, hash_pairs)
   
   # extract features of new pairs
-  if(verbose) message("MESSAGE")
   feature_mat_tmp <- .extract_features(dat, pairs_mat_new, fl)
   if(any(is.na(feature_mat_tmp))){
    feature_mat_tmp <- .clean_na(feature_mat_tmp, hash_na, offset = counter)
@@ -48,12 +56,14 @@ dvisR_system <- function(dat, cluster_labels = rep(NA, nrow(dat)),
    idx_vec <- sort(idx_vec)
    .check_indices(idx_vec, pairs_mat, hash_pairs)
    
-   #plot according to filter
+   ## plot according to filter
    pairs_submat <- pairs_mat[idx_vec,]
-   .plotter_first_phase(dat, pairs_submat, plotting_options = po, plotting_module = pm, i = round_idx, ...)
+   .plotter_first_phase(dat, pairs_submat, plotting_options = po, plotting_module = pm, i = round_idx, 
+                        debugging_inputs = di, ...)
    
-   # display and listen
-   response_vec[idx_vec] <- .response_listener_first_phase(round_idx, number_requested)
+   ## display and listen
+   response_vec[idx_vec] <- .response_listener_first_phase(round_idx, number_requested, debugging_inputs = di)
+   round_vec[idx_vec] <- round_idx
   }
   
   # do phase 2 learning -- user labels one plot at a time
@@ -63,12 +73,14 @@ dvisR_system <- function(dat, cluster_labels = rep(NA, nrow(dat)),
                                          options = so$learner_options$second_learner)
    .check_indices(idx, pairs_mat, hash_pairs)
    
-   #plot according to filter
+   ## plot according to filter
    pair_vec <- pairs_mat[idx,]
-   .plotter_second_phase(dat, pair_vec, plotting_options = po, plotting_module = pm, i = round_idx, ...)
+   .plotter_second_phase(dat, pair_vec, plotting_options = po, plotting_module = pm, i = round_idx, 
+                         debugging_inputs = di, ...)
    
-   # display and listen
-   response_vec[idx] <- .response_listener_second_phase(round_idx)
+   ## display and listen
+   response_vec[idx] <- .response_listener_second_phase(round_idx, debugging_inputs = di)
+   round_idx[idx] <- round_idx
   }
    
   round_idx <- round_idx+1
@@ -78,8 +90,12 @@ dvisR_system <- function(dat, cluster_labels = rep(NA, nrow(dat)),
  classifier <- system_options$classifier(data = feature_mat, label = response_vec)
  
  # prepare output
+ feature_mat <- .clean_feature_matrix(feature_mat)
  feature_mat <- .restore_na(feature_mat, hash_na)
- .construct_dvisR(feature_mat, response_vec, classifier, so)
+ response_vec <- .clean_response_vec(response_vec)
+ round_vec <- .clean_response_vec(round_vec)
+ 
+ .construct_dvisR(feature_mat, response_vec, pairs_mat, round_vec, classifier, so)
 }
 
 ####################
@@ -92,8 +108,10 @@ dvisR_system <- function(dat, cluster_labels = rep(NA, nrow(dat)),
  invisible()
 }
 
-.plotter_first_phase <- function(dat, pairs_submat, plotting_options, plotting_module, i, ...){
- if(!all(is.na(plotting_options$first_mar))){
+.plotter_first_phase <- function(dat, pairs_submat, plotting_options, plotting_module, i, debugging_inputs = NA, ...){
+  if(is.list(debugging_inputs) || all(!is.na(debugging_inputs))) return(invisible())
+  
+  if(!all(is.na(plotting_options$first_mar))){
   graphics::par(mar = plotting_options$first_mar)
  }
  
@@ -113,14 +131,16 @@ dvisR_system <- function(dat, cluster_labels = rep(NA, nrow(dat)),
   }
   
   plotting_module(x = dat[,pairs_submat[j,1]], y = dat[,pairs_submat[j,2]], xlab = xlab, ylab = ylab,
-                  main = paste0("Round ", i, ": Plot \#", j), color_vec = color_vec, ...)
+                  main = paste0("Round ", i, ": Plot #", j), color_vec = color_vec, ...)
  }
  
  invisible()
 }
 
-.plotter_second_phase <- function(dat, pair_vec, plotting_options, plotting_module, i, ...){
- if(!all(is.na(plotting_options$second_mar))){
+.plotter_second_phase <- function(dat, pair_vec, plotting_options, plotting_module, i, debugging_inputs = NA, ...){
+  if(is.list(debugging_inputs)  || all(!is.na(debugging_inputs))) return(invisible())
+  
+  if(!all(is.na(plotting_options$second_mar))){
   graphics::par(mar = plotting_options$second_mar)
  }
   
@@ -142,16 +162,21 @@ dvisR_system <- function(dat, cluster_labels = rep(NA, nrow(dat)),
  invisible()
 }
 
-.response_listener_first_phase <- function(i, number_requested){
- while(TRUE){
-  user_input <- readline(paste0("Round ", i, ": Enter the plot #'s that ",
-                                " contain patterns of interest (space-separated): "))
+.response_listener_first_phase <- function(i, number_requested, debugging_inputs){
+  if((is.list(debugging_inputs)  || all(!is.na(debugging_inputs))) & length(debugging_inputs$round_inputs) >= 1){
+    response <- .response_handler_first_phase(debugging_inputs$round_inputs[[i]], number_requested)
+    
+  } else {
+    while(TRUE){
+      user_input <- readline(paste0("Round ", i, ":\nEnter the plot #'s that ",
+                                    " contain patterns of interest (space-separated): "))
+      
+      response <- .response_handler_first_phase(user_input, number_requested)
+      if(!is.na(response)) break
+    }
+  }
   
-  response <- .response_handler_first_phase(user_input, number_requested)
-  if(!is.na(response)) break
- }
- 
- response
+  response
 }
 
 .response_handler_first_phase <- function(user_input, number_requested){
@@ -169,14 +194,19 @@ dvisR_system <- function(dat, cluster_labels = rep(NA, nrow(dat)),
  response
 }
 
-.response_listener_second_phase <- function(i){
- while(TRUE){
-  user_input <- readline(paste0("Round ", i, ": Enter 'y' or 'n' on if plot",
-                                " contains patterns of interest: "))
-  
-  response <- .response_handler_second_phase(user_input)
-  if(!is.na(response)) break
- }
+.response_listener_second_phase <- function(i, debugging_inputs){
+  if((is.list(debugging_inputs)  || all(!is.na(debugging_inputs))) & length(debugging_inputs$round_inputs) >= 1){
+    response <- .response_handler_first_phase(debugging_inputs$round_inputs[[i]], number_requested)
+    
+  } else {
+    while(TRUE){
+      user_input <- readline(paste0("Round ", i, ":\nEnter 'y' or 'n' if the plot",
+                                    " contains patterns of interest: "))
+      
+      response <- .response_handler_second_phase(user_input)
+      if(!is.na(response)) break
+    }
+  }
  
  response
 }
@@ -193,15 +223,15 @@ dvisR_system <- function(dat, cluster_labels = rep(NA, nrow(dat)),
  response
 }
 
- 
 #####################
 
-.construct_dvisR <- function(feature_mat, response_vec, pairs_mat, classifier, system_options){
+.construct_dvisR <- function(feature_mat, response_vec, pairs_mat, round_vec, 
+                             classifier, system_options){
   stopifnot(is.function(classifier), nrow(feature_mat) == length(response_vec),
             length(response_vec) == nrow(pairs_mat))
   
-  df <- as.data.frame(cbind(pairs_mat, response_vec, feature_mat))
-  colnames(df) <- c("Idx1", "Idx2", "Response", colnames(feature_mat))
+  df <- as.data.frame(cbind(pairs_mat, response_vec, round_vec, feature_mat))
+  colnames(df) <- c("Idx1", "Idx2", "Response", "Round", colnames(feature_mat))
   
   structure(list(df = df, classifier = classifier), class = "dvisR")
 }
