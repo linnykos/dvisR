14827816cb59a5989487536176ed624a7dba8b73
Kevin Lin <klin1234@gmail.com>
2020-09-24 21:30:29 -0400

    Added the preliminary heatmap, but much to be improved still

--- dvisR/R/dvisR_heatmap.R
+++ dvisR/R/dvisR_heatmap.R
@@ -1,4 +1,4 @@
-dvisR_heatmap <- function(obj, reorder_feature = T, predictions = T, unlabeled = T,
+dvisR_heatmap <- function(obj, reorder_feature = T, predictions = T, unlabeled = F,
                           size_func = heatmap_size_func_default,
                           prediction_func = predict_xgboost_wrapper,
                           feature_spacing = 5, ylab = NA,
@@ -43,17 +43,6 @@ dvisR_heatmap <- function(obj, reorder_feature = T, predictions = T, unlabeled =
                       row_order = row_order, col_order = col_order, 
                       col_palette = col_palette, par_list = par_list)
  
- #add vertical lines
- idx <- sum(x$response)
- row.spacing <- 1/(n-1)
- graphics::lines(x = c(-1,2), y = rep(idx*row.spacing - row.spacing/2, 2), lwd = 4)
- 
- #add horizontal lines
- col.spacing <- 1/d
- graphics::lines(x = rep(col.spacing - col.spacing/2, 2), y = c(-1, 2), lwd = 4)
- 
- list(col.order = colnames(x$feature.matrix)[col.order],
-      row.order = row.order)
 }
 
 heatmap_size_func_default <- function(vec){
@@ -81,11 +70,12 @@ heatmap_par_list_default <- function(col_pos = col_palette_default("green"),
                                      max_size = 0.95,
                                      lwd_separator = 2, lty_separator = 1,
                                      lwd_spacing = 0.5, lty_spacing = 2,
-                                     cex = 1){
+                                     cex = 1, response_width = 1){
  list(col_pos = col_pos, col_neg = col_neg, col_na = col_na, col_big = col_big, col_small = col_small, 
       col_bg = col_bg, col_other = col_other,
       max_size = max_size,
-      lwd_separator = lwd_separator, lty_separator = 1, lwd_spacing = lwd_spacing, lty_spacing = lty_spacing, cex = cex)
+      lwd_separator = lwd_separator, lty_separator = 1, lwd_spacing = lwd_spacing, lty_spacing = lty_spacing, 
+      cex = cex, response_width = response_width)
 }
 
 ##############################
@@ -121,28 +111,24 @@ heatmap_par_list_default <- function(col_pos = col_palette_default("green"),
  
  # compute preliminaries
  n <- nrow(mat); p <- ncol(mat)
- if(all(is.na(pred_vec))) xlim <- c(0, p+1) else xlim <- c(0, p+2)
+ if(all(is.na(pred_vec))) xlim <- c(0, p+pl$response_width) else xlim <- c(0, p+2*pl$response_width)
  ylim <- c(0,n)
  
  # compute asp
  
- graphics::plot(NA, xlim = xlim, ylim = ylim) #, xaxt = "n", yaxt = "n", xlab = "", ylab = "", bty = "n")
+ graphics::plot(NA, xlim = xlim, ylim = ylim, xaxt = "n", yaxt = "n", xlab = "", ylab = "", bty = "n")
  
- # usr <- graphics::par("usr")
- # if(abs(usr[1]-usr[2]) < abs(usr[3]-usr[4])){
- #  graphics::par(usr = c(xlim[1], xlim[2], usr[3], usr[4]))
- # } else {
- #  graphics::par(usr = c(usr[1], usr[2], ylim[1], ylim[2]))
- # }
+ usr <- graphics::par("usr")
  
  # plot response and prediction
  .plot_response(response_vec, row_order = row_order, col_order = col_order, par_list = pl, 
-                ylim = ylim, offset = 0)
+                ylim = ylim, offset = 0, width = pl$response_width)
  if(!all(is.na(pred_vec))) .plot_response(pred_vec, row_order = row_order, col_order = col_order, 
-                                          ylim = ylim, par_list = pl, offset = 1)
+                                          ylim = ylim, par_list = pl, offset = pl$response_width,
+                                          width = pl$response_width)
  
  # draw background
- offset <- ifelse(!all(is.na(pred_vec)), 2, 1)
+ offset <- ifelse(!all(is.na(pred_vec)), 2*pl$response_width, 1*pl$response_width)
  graphics::rect(offset, ylim[1], xlim[2], ylim[2], col = pl$col_bg, border = NA)
  
  # plot squares
@@ -153,17 +139,28 @@ heatmap_par_list_default <- function(col_pos = col_palette_default("green"),
    col <- ifelse(mat[i,j] >= 0, pl$col_big, pl$col_small)
    x_mid <- offset+(j-1)+0.5 
    y_mid <- ylim[2]-(i-1)-0.5 
-   print(x_mid)
-   print(y_mid)
-   print("----")
    graphics::rect(x_mid-half_width, y_mid-half_width, x_mid+half_width, y_mid+half_width, col = col, border = NA)
   }
  }
  
+ # add default vertical lines
+ graphics::lines(x = rep(pl$response_width, 2), y = ylim,
+                 col = pl$col_other, lwd = pl$lwd_separator, lty = pl$lty_separator)
+ if(!all(is.na(pred_vec))) graphics::lines(x = rep(2*pl$response_width, 2), y = ylim,
+                                 col = pl$col_other, lwd = pl$lwd_separator, lty = pl$lty_separator)
+ 
+ # add horizontal lines
+ if(length(names(row_order)) > 1){
+  num_pos <- length(which(response_vec == 1))
+  graphics::lines(x = xlim, y = rep(ylim[2] - num_pos, 2),
+                  col = pl$col_other, lwd = pl$lwd_separator, lty = pl$lty_separator)
+ }
+ 
+ 
  invisible()
 }
 
-.plot_response <- function(vec, row_order, col_order, par_list, ylim, offset = 0){
+.plot_response <- function(vec, row_order, col_order, par_list, ylim, offset = 0, width = 1){
  pl <- par_list
  
  #draw the response
@@ -178,7 +175,7 @@ heatmap_par_list_default <- function(col_pos = col_palette_default("green"),
    stop()
   }
   
-  graphics::rect(offset, ylim[2]-(i-1), offset+1, ylim[2]-i, col = col, border = NA)
+  graphics::rect(offset, ylim[2]-(i-1), offset+width, ylim[2]-i, col = col, border = NA)
  }
 
  invisible()
--- experiment/experiment.R
+++ experiment/experiment.R
@@ -16,56 +16,58 @@ res_safe <- res
 
 par(mar = c(4,4,4,1))
 
-obj = res_safe
-reorder_feature = T
-predictions = T
-unlabeled = F
-size_func = heatmap_size_func_default
-prediction_func = predict_xgboost_wrapper
-feature_spacing = 5
-ylab = NA
-par_list = heatmap_par_list_default()
-
-###########
-
-res <- .extract_mat_response(obj)
-n <- nrow(res$feature_mat); p <- ncol(res$feature_mat)
-
-# remove unlabeled pairs if needed
-if(!unlabeled){
- idx <- which(is.na(res$response_vec))
- if(length(idx) > 0){
-  res$feature_mat <- res$feature_mat[-idx,,drop = F]
-  res$response_vec <- res$response_vec[-idx]
- }
-}
-
-# compute sizes of boxes in the plot
-size_mat <- apply(res$feature_mat, 2, size_func)
-stopifnot(all(abs(size_mat) <= 1), sum(is.na(size_mat)) == 0)
-
-# compute ordering for the plot
-if(reorder_feature){
- col_order <- .hclust_col_ordering(size_mat)
- row_order <- .hclust_row_ordering(size_mat, res$response_vec)
-} else {
- col_order <- 1:p; row_order <- 1:n
-}
-
-# compute predictions
-if(predictions){
- tmp <- prediction_func(obj$classifier, res$feature_mat)
- pred_vec <- rep(0, length(tmp))
- pred_vec[tmp >= 1/2] <- 1
-} else {
- pred_vec <- NA
-}
-
-# draw the plot
-.draw_feature_matrix(mat = size_mat, response_vec = res$response_vec, pred_vec = pred_vec,
-                     row_order = row_order, col_order = col_order,
-                     col_palette = col_palette, par_list = par_list)
-
-
-
-
+dvisR_heatmap(res_safe)
+# 
+# obj = res_safe
+# reorder_feature = T
+# predictions = T
+# unlabeled = F
+# size_func = heatmap_size_func_default
+# prediction_func = predict_xgboost_wrapper
+# feature_spacing = 5
+# ylab = NA
+# par_list = heatmap_par_list_default()
+# 
+# ###########
+# 
+# res <- .extract_mat_response(obj)
+# n <- nrow(res$feature_mat); p <- ncol(res$feature_mat)
+# 
+# # remove unlabeled pairs if needed
+# if(!unlabeled){
+#  idx <- which(is.na(res$response_vec))
+#  if(length(idx) > 0){
+#   res$feature_mat <- res$feature_mat[-idx,,drop = F]
+#   res$response_vec <- res$response_vec[-idx]
+#  }
+# }
+# 
+# # compute sizes of boxes in the plot
+# size_mat <- apply(res$feature_mat, 2, size_func)
+# stopifnot(all(abs(size_mat) <= 1), sum(is.na(size_mat)) == 0)
+# 
+# # compute ordering for the plot
+# if(reorder_feature){
+#  col_order <- .hclust_col_ordering(size_mat)
+#  row_order <- .hclust_row_ordering(size_mat, res$response_vec)
+# } else {
+#  col_order <- 1:p; row_order <- 1:n
+# }
+# 
+# # compute predictions
+# if(predictions){
+#  tmp <- prediction_func(obj$classifier, res$feature_mat)
+#  pred_vec <- rep(0, length(tmp))
+#  pred_vec[tmp >= 1/2] <- 1
+# } else {
+#  pred_vec <- NA
+# }
+# 
+# # draw the plot
+# .draw_feature_matrix(mat = size_mat, response_vec = res$response_vec, pred_vec = pred_vec,
+#                      row_order = row_order, col_order = col_order,
+#                      col_palette = col_palette, par_list = par_list)
+# 
+# 
+# 
+# 
