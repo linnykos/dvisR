cda98e87040ba08fdc944855ae13dea0da6d4923
Kevin Lin <klin1234@gmail.com>
2020-10-21 23:52:55 -0400

    Adding the README

---
+++ README.md
@@ -0,0 +1,2 @@
+[![Build Status](https://travis-ci.org/linnykos/dvisR.svg?branch=master)](https://travis-ci.org/linnykos/dvisR)
+[![codecov](https://codecov.io/gh/linnykos/dvisR/branch/master/graph/badge.svg)](https://codecov.io/gh/linnykos/dvisR)
--- experiment/experiment.R
+++ experiment/experiment.R
@@ -13,26 +13,26 @@ res_safe <- res
 
 # plot the feature-pairs that the system touched
 par(mfrow = c(1,2))
-dvisR_embedding(res, embedding_method = embedding_umap, seed = 1, main = "Provided labels")
+dvisR_embedding(res, embedding_method = embedding_umap, seed = 1, main = "Provided labels",
+                legend = embedding_legend(x = "bottomleft", cex = 0.5))
 dvisR_embedding(res, embedding_method = embedding_umap, quantity_plotted = "probability", seed = 1,
-                main = "Predicted labels")
+                main = "Predicted labels", legend = embedding_legend(x = "bottomleft", cex = 0.5))
 
 # plot all the remaining feature-pairs
 pred_res <- dvisR_prediction(res, dat) # this might take a while
 par(mfrow = c(1,2))
 dvisR_embedding(pred_res, embedding_method = embedding_umap, seed = 1, main = "Provided labels", 
-                legend = embedding_legend(cex = 0.5))
+                legend = embedding_legend(x = "bottomleft", cex = 0.5))
 dvisR_embedding(pred_res, embedding_method = embedding_umap, quantity_plotted = "probability", seed = 1,
-                main = "Predicted labels", legend = embedding_legend(cex = 0.5))
+                main = "Predicted labels", legend = embedding_legend(x = "bottomleft", cex = 0.5))
 
 # we can also highlight specific variables
 par(mfrow = c(1,2))
 dvisR_embedding(pred_res, embedding_method = embedding_umap, highlight_variable = 2, seed = 1, 
-                main = "Provided labels", legend = embedding_legend(cex = 0.5))
+                main = "Provided labels", legend = embedding_legend(x = "bottomleft", cex = 0.5))
 dvisR_embedding(pred_res, embedding_method = embedding_umap, quantity_plotted = "probability",
                 highlight_variable = 2, seed = 1, main = "Predicted labels", 
-                legend = embedding_legend(cex = 0.5))
-
+                legend = embedding_legend(x = "bottomleft", cex = 0.5))
 
 par(mar = c(0.5, 0.5, 0.5, 0.5), mfrow = c(1,1))
 dvisR_heatmap(res)
